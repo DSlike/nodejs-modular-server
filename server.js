4179a172563294774e5929c271763f2cc4742592
Daniel <danyjunor96@gmail.com>
2019-06-06 21:40:37 +0300

    Clustering server-app by default

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 'use strict';
 
-const app = require('./app/core/app.js');
+const cluster = require('cluster');
 
 if (!process.env.NODE_ENV)
   process.env.NODE_ENV = 'development';
 
-require('./env');
-require('./app/core/router.js')(app);
+const numCPUs = process.env.NODE_ENV == 'development' ? 1 : require('os').cpus().length * 2;
+
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+
+} else {
+  const app = require('./app/core/app.js');
+
+  require('./env');
+  require('./app/core/router.js')(app);
+}
